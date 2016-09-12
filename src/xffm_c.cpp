a68049624ab0033b88b9c00684a6f087a2d190e9
Dr. Edscott Wilson Garcia <edscott.wilson.garcia@gmail.com>
2016-09-11 19:30:27 -0500

    Prepare final steps for enabling signal callbacks
    Check FIXMEs in xffm_c class for pending issues to finish this
    point up.

--- src/xffm_c.cpp
+++ src/xffm_c.cpp
@@ -1,3 +1,6 @@
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
 #include "xffm_c.hpp"
 #include "window_c.hpp"
 static void activate(GtkApplication *, void *);
@@ -92,7 +95,8 @@ process_info (GSimpleAction *action,
 {
     DBG("process_info\n");
 }
-
+// FIXME: one callback for each signal. Yeah, that is not quite
+//        efficient, but what the heck...
 static GActionEntry app_entries[] =
 {
   { "send_signal", send_signal, NULL, NULL, NULL },
@@ -218,6 +222,97 @@ open (GtkApplication *app,
 void
 xffm_c::remove_window_p_from_list(void *data){
     window_p_list = g_list_remove(window_p_list, data);
+}
+
+static 
+void
+ps_signal(gint pid, gint signal_id){
+    if (!pid) return;
+    // Is the effective runtime uid the same as that for the process to signal?
+    
+    // 1. Are we executing the command in a shell chain?
+    gchar *pcommand = g_strdup_printf("ps ax -o ppid,pid");
+    FILE *p = popen(pcommand, "r");
+    if (!p){
+        g_warning("pipe creation failed for %s\n", pcommand);
+        g_free(pcommand);
+        return;
+    } 
+    g_free(pcommand);
+    gint cpid = -1;
+    gchar *spid = g_strdup_printf("%0d", pid);
+    gchar buffer[64];
+    memset(buffer, 0, 64);
+    while (fgets(buffer, 63, p) && !feof(p)){
+        if (strncmp(buffer, spid, strlen(spid))==0){
+            DBG("gotcha: %s", buffer);
+            gchar **gg=g_strsplit(buffer, " ", -1);
+            errno = 0;
+            long l = strtol(gg[1], NULL, 10);
+            if (errno) {
+                g_warning("cannot parse to long: %s\n", gg[1]);
+                pclose(p);
+                g_free(spid);
+                g_strfreev(gg);
+                return;
+            }
+            cpid = l;
+            g_strfreev(gg);
+            break;
+        }
+    }
+    pclose(p);
+    // If cpid turns out > 0, then we are in a chained command and pid must change
+    if (cpid > 0) {
+        pid = cpid;
+        g_free(spid);
+        spid = g_strdup_printf("%0d", pid);
+    }
+
+    // 2. Does pid (either direct or from chained command) belong to us?
+    pcommand = g_strdup_printf("ps -p %d -o uid", (int)pid);
+    gboolean sudoize = FALSE;
+    uid_t uid = geteuid();
+    p = popen(pcommand, "r");
+    long luid = geteuid();
+    if (!p){
+        g_warning("pipe creation failed for %s\n", pcommand);
+    } else {
+        gchar buffer[64];
+        memset(buffer, 0, 64);
+        while (fgets(buffer, 63, p) && !feof(p)){
+	    if (strstr(buffer, "UID")) continue;
+            errno=0;
+            luid = strtol(buffer, NULL, 10);
+            if (!errno){
+		DBG("line: %s gotcha: %ld\n", buffer, luid);
+                if (luid != uid) sudoize = TRUE;
+                break;
+            }
+        }
+        pclose(p);
+    }
+    g_free(pcommand);
+    gchar *sudo = g_find_program_in_path("sudo");
+    DBG("signal to pid: %s (owned by %d, sudo=%d)\n", spid, (int)luid, sudoize);
+    gint sig=signal_id;
+    if (sudoize && sudo) {
+        DBG("ps_signal by means of sudo...\n");
+	// FIXME
+        /*widgets_t *widgets_p = rfm_get_widget ("widgets_p");
+        gchar *signal_number = g_strdup_printf("-%0d", sig);
+        gchar *argv[]={sudo, "-A", "kill", signal_number, spid, NULL};
+        rfm_thread_run_argv (widgets_p, argv, FALSE);
+        g_free(signal_number);*/
+    } else {
+        if (!sudo) g_warning("sudo command not found to signal non-owned process\n");
+        DBG("normal ps_signal to %d...\n", (int)pid);
+        kill(pid, sig);
+    }
+    //rfm_rational(RFM_MODULE_DIR, "callbacks", GINT_TO_POINTER(REFRESH_ACTIVATE), NULL, "callback");
+    g_free(sudo);
+    g_free(spid);
+    return;
 }
 
 
