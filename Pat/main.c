65a2378b410187eb36c25bb1753101db2245a8b4
Dr. Edscott Wilson Garcia <edscott.wilson.garcia@gmail.com>
2017-03-03 20:43:13 -0600

    Pat fixes

--- Pat/main.c
+++ Pat/main.c
@@ -14,12 +14,25 @@ static    gint answer;
 static    gint right_answers = 0;
 static    gint wrong_answers = 0;
 static    gint score = 0;
-static    GtkWidget *question;
+static    GtkWidget *happy;
 static    GtkWidget *wrong;
 static    GtkLabel *operation;
 static    GtkLabel *score_label;
 static    GtkLabel *last_op_label;
-static time_t start;
+static 	  GtkWidget *last_op_box;
+static time_t start=0;
+
+gchar *operation_string=NULL;
+
+static void
+set_last_result(void){
+    if (!operation_string) return;
+    gchar *g = g_strdup_printf("       %s  <span color=\"green\">%d</span>     ",
+	    operation_string,answer);
+    gtk_label_set_markup(last_op_label, g);
+    g_free(g);
+    gtk_widget_show(last_op_box);
+}
 
 static void set_operation(void){
     gint r = rand(); 
@@ -29,20 +42,21 @@ static void set_operation(void){
     gint r2 = (rand() % 10) +1; 
     if (r) {
 	// multiplication
-	gchar *g = g_strdup_printf("<b>%d <span color=\"blue\">X</span> %d  = </b>", r1, r2);
+	g_free(operation_string);
+	operation_string = g_strdup_printf("<b>%d <span color=\"blue\">X</span> %d  = </b>", r1, r2);
 	answer=r1*r2;
-	gtk_label_set_markup(operation, g);
-	g_free(g);
+	gtk_label_set_markup(operation, operation_string);
     } else {
 	// division
 	answer=r1;
-	gchar *g = g_strdup_printf("<b>%d <span color=\"blue\">/</span> %d = </b>", r1*r2, r2);
-	gtk_label_set_markup(operation, g);
-	g_free(g);
+	g_free(operation_string);
+	operation_string = g_strdup_printf("<b>%d <span color=\"blue\">/</span> %d = </b>", r1*r2, r2);
+	gtk_label_set_markup(operation, operation_string);
     }
 }
 
 static void set_score(void){
+    if (start == 0) start = time(NULL);
     gchar *t = g_strdup_printf("%d:%02d", (time(NULL)-start)/60, (time(NULL)-start)%60);
     gchar *g = g_strdup_printf("correctos = <span color=\"green\">%d</span> incorrectos = <span color=\"red\">%d</span> nivel = <span color=\"blue\">%d</span>\n tiempo = %s", right_answers, wrong_answers, score, t);
     gtk_label_set_markup(score_label, g);
@@ -56,12 +70,13 @@ static void check_answer(GtkWidget *entry, void *data){
     gint x = atoi(s);
     if (x==answer){
 	gtk_widget_hide(wrong);
-	gtk_widget_show(question);
+	set_last_result();
+	//gtk_widget_show(happy);
 	set_operation();
 	right_answers++;
 	score++;
     } else {
-	gtk_widget_hide(question);
+	//gtk_widget_hide(happy);
 	gtk_widget_show(wrong);
 	wrong_answers++;
 	score--;
@@ -77,8 +92,13 @@ static void check_answer(GtkWidget *entry, void *data){
     }
 }
 
+static void
+delete_event (GtkWidget * widget, GdkEvent * event, gpointer data) {
+    gtk_main_quit();
+    exit(1);
+}
+
 gint main(gint argc, gchar **argv){
-    start = time(NULL);
     gtk_init (&argc, &argv);
     srand(time(NULL));   // should only be called once
     if (argv[1]) goal=atoi(argv[1]); else goal=GOAL;
@@ -88,10 +108,10 @@ gint main(gint argc, gchar **argv){
     operation = GTK_LABEL(gtk_label_new(""));
     GtkEntry *answer = GTK_ENTRY(gtk_entry_new());
 
-    question = gtk_image_new_from_icon_name ("face-surprise", GTK_ICON_SIZE_SMALL_TOOLBAR);
+    happy = gtk_image_new_from_icon_name ("face-laugh", GTK_ICON_SIZE_SMALL_TOOLBAR);
     wrong = gtk_image_new_from_icon_name ("edit-delete", GTK_ICON_SIZE_SMALL_TOOLBAR);
 
-    GtkWidget *last_op_box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+    last_op_box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
     GtkWidget *score_box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
     last_op_label = GTK_LABEL(gtk_label_new(""));
     score_label = GTK_LABEL(gtk_label_new(""));
@@ -99,14 +119,19 @@ gint main(gint argc, gchar **argv){
 
     gtk_container_add (GTK_CONTAINER (window), vbox);
     gtk_box_pack_start (GTK_BOX (vbox), hbox, FALSE, FALSE, 0);
+    gtk_box_pack_end (GTK_BOX (vbox), GTK_WIDGET(last_op_box), FALSE, FALSE, 0);
     gtk_box_pack_start (GTK_BOX (vbox), GTK_WIDGET(score_box), TRUE, TRUE, 0);
     gtk_box_pack_start (GTK_BOX (hbox), GTK_WIDGET(operation), TRUE, TRUE, 0);
     gtk_box_pack_start (GTK_BOX (hbox), GTK_WIDGET(answer), TRUE, TRUE, 0);
 
-    gtk_box_pack_start (GTK_BOX (hbox), question, TRUE, TRUE, 0);
+    GtkWidget *aux_box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+    gtk_box_pack_start (GTK_BOX (last_op_box), GTK_WIDGET(aux_box), TRUE, TRUE, 0);
+
+    gtk_box_pack_start (GTK_BOX (aux_box), GTK_WIDGET(last_op_label), FALSE, FALSE, 0);
+    gtk_box_pack_start (GTK_BOX (aux_box), happy, FALSE, FALSE, 0);
     gtk_box_pack_start (GTK_BOX (hbox), wrong, TRUE, TRUE, 0);
 
-    gtk_box_pack_start (GTK_BOX (score_box), GTK_WIDGET(last_op_box), TRUE, TRUE, 0);
+
 
     gtk_box_pack_start (GTK_BOX (score_box), GTK_WIDGET(score_label), TRUE, TRUE, 0);
 
@@ -114,11 +139,16 @@ gint main(gint argc, gchar **argv){
     set_score();
     gtk_widget_show_all(window);
     gtk_widget_hide(wrong);
-    gtk_widget_hide(question);
+    gtk_widget_hide(last_op_box);
    
     g_signal_connect (answer, "activate", 
             G_CALLBACK (check_answer), score_label);
-
+    g_signal_connect (G_OBJECT (window), "delete_event",
+	    G_CALLBACK (delete_event), (gpointer) NULL);
+    g_signal_connect (G_OBJECT (window), "destroy", 
+	    G_CALLBACK (delete_event), (gpointer) NULL);
+    g_signal_connect (G_OBJECT (window), "destroy_event",
+	    G_CALLBACK (delete_event), (gpointer) NULL);
     gtk_main();
     gtk_widget_hide(window);
 
