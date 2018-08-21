cf25045858a27176fb353c5d30ac9e3ac9f135f4
edscott wilson garcia <edscott@imp.mx>
2018-08-21 17:31:15 -0500

    missing files

---
+++ common/hbuttonbox.hh
@@ -0,0 +1,64 @@
+#ifndef XF_HBUTTONBOX
+#define XF_HBUTTONBOX
+
+namespace xf {
+
+template <class Type>
+class HButtonBox {
+public:
+    HButtonBox(void){
+	// The box
+	hButtonBox_ = GTK_BOX(gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0));
+	// Term input (status) icon.
+	auto status_icon = GTK_IMAGE(gtk_image_new_from_icon_name ("utilities-terminal", GTK_ICON_SIZE_SMALL_TOOLBAR)); 
+	 g_object_set_data(G_OBJECT(hButtonBox_), "status_icon", status_icon);
+	// Iconview icon
+	auto iconview_icon = GTK_IMAGE(gtk_image_new_from_icon_name ("system-file-manager", GTK_ICON_SIZE_SMALL_TOOLBAR)); 
+	 g_object_set_data(G_OBJECT(hButtonBox_), "iconview_icon", iconview_icon);
+	// Status textview
+	auto status = createStatus();
+	 g_object_set_data(G_OBJECT(hButtonBox_), "status", status);
+
+	auto statusBox = GTK_BOX(gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0));
+
+	auto status_button = GTK_BUTTON(gtk_button_new());
+	auto status_label = GTK_LABEL(gtk_label_new (""));
+        gchar *g = g_strdup_printf("<span color=\"blue\"><b>%s-%s</b></span>",
+                "xffm+", VERSION);
+        gtk_label_set_markup(status_label,g);
+        g_free(g);
+	 g_object_set_data(G_OBJECT(statusBox), "status_label", status_label);
+	
+        gtk_box_pack_start (hButtonBox_, GTK_WIDGET(status_icon), FALSE, FALSE, 5);
+        gtk_box_pack_start (hButtonBox_, GTK_WIDGET(iconview_icon), FALSE, FALSE, 5);
+        gtk_box_pack_start (hButtonBox_, GTK_WIDGET(status), TRUE, TRUE, 0);
+        gtk_box_pack_start (hButtonBox_, GTK_WIDGET(status_button), TRUE, TRUE, 0);
+        gtk_container_add (GTK_CONTAINER (status_button), GTK_WIDGET(statusBox));
+        gtk_box_pack_start (statusBox, GTK_WIDGET(status_label), FALSE, FALSE, 0);
+	gtk_widget_show_all(GTK_WIDGET(statusBox));
+	gtk_widget_show_all(GTK_WIDGET(hButtonBox_));
+        return;  
+    }
+    
+
+
+    GtkBox *hButtonBox(void){return hButtonBox_;}
+
+private:
+    GtkBox *hButtonBox_;
+    GtkTextView *createStatus(void){
+	auto status = GTK_TEXT_VIEW(gtk_text_view_new ());
+	gtk_text_view_set_pixels_above_lines (status, 10);
+	gtk_text_view_set_monospace (status, TRUE);
+	gtk_text_view_set_editable (status, TRUE);
+	gtk_text_view_set_cursor_visible (status, TRUE);
+	gtk_text_view_place_cursor_onscreen(status);
+	gtk_text_view_set_wrap_mode (status, GTK_WRAP_CHAR);
+	gtk_widget_set_can_focus(GTK_WIDGET(status), TRUE);
+	gtk_widget_show_all(GTK_WIDGET(status));
+	return status;
+    }
+
+ };
+}
+#endif
---
+++ common/pagechild.hh
@@ -0,0 +1,95 @@
+#ifndef XF_PAGE_CHILD
+#define XF_PAGE_CHILD
+#include "vbuttonbox.hh"
+#include "hbuttonbox.hh"
+#include "vpane.hh"
+
+namespace xf{
+
+template <class Type>
+class PageChild: public Vpane<Type>, VButtonBox<Type>, HButtonBox<Type>{
+    using gtk_c = Gtk<double>;
+public:
+
+    PageChild(void){
+	pageChild_ = GTK_BOX(gtk_box_new (GTK_ORIENTATION_VERTICAL, 0));
+	pageLabel_ = GTK_LABEL(gtk_label_new ("foobar"));
+	pageLabelBox_ = GTK_BOX(gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0));
+	pageLabelSpinner_ = GTK_SPINNER(gtk_spinner_new());
+	pageLabelSpinnerBox_ = GTK_BOX(gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0));
+	pageLabelIconBox_ = GTK_BOX(gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0));
+	pageLabelButton_ = GTK_BUTTON(gtk_button_new ());
+
+        // FIXME: this goes in term::dialog:
+        //gtk_c::set_bin_image(GTK_WIDGET(pageLabelIconBox_), "utilities-terminal", GTK_ICON_SIZE_SMALL_TOOLBAR);
+
+	gtk_box_pack_start (pageLabelBox_, GTK_WIDGET(pageLabelSpinnerBox_), TRUE, TRUE, 0);
+	gtk_box_pack_start (pageLabelSpinnerBox_, GTK_WIDGET(pageLabelSpinner_), TRUE, TRUE, 0);
+	gtk_box_pack_start (pageLabelBox_, GTK_WIDGET(pageLabelIconBox_), TRUE, TRUE, 0);
+	gtk_box_pack_start (pageLabelBox_, GTK_WIDGET(pageLabel_), TRUE, TRUE, 2);
+	gtk_box_pack_end (pageLabelBox_, GTK_WIDGET(pageLabelButton_), TRUE, TRUE, 0);
+        gtk_c::setup_image_button(GTK_WIDGET(pageLabelButton_), "window-close", _("Close"));
+    
+	//set_spinner(pageSpinner_, TRUE, pageLabelIconBox_);
+	set_spinner(FALSE);
+
+	// pathbar should be created with pathbar_c object.
+	// FIXME: add pathbar
+	// gtk_box_pack_start (pageChild, pathbar_p->get_pathbar(), FALSE, FALSE, 0);
+	gtk_widget_show_all(GTK_WIDGET(pageLabelBox_));
+
+        GtkBox *hViewBox = GTK_BOX(gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0));
+        GtkBox *hButtonBox = this->hButtonBox();
+        GtkBox *vButtonBox = this->vButtonBox();
+        GtkPaned *vpane = this->vpane();
+	gtk_box_pack_start (hViewBox, GTK_WIDGET(vpane), TRUE, TRUE, 0);
+	gtk_box_pack_start (hViewBox, GTK_WIDGET(vButtonBox), FALSE, FALSE, 0);
+	gtk_box_pack_start (pageChild_, GTK_WIDGET(hViewBox), TRUE, TRUE, 0);
+	gtk_box_pack_start (pageChild_, GTK_WIDGET(hButtonBox), FALSE, FALSE, 0);
+
+	gtk_widget_show_all(GTK_WIDGET(pageChild_));
+
+	return;
+
+    }
+    void setPageLabel(const gchar *text){
+         gtk_label_set_markup(pageLabel_, text);
+         //gtk_notebook_set_tab_label(notebook_, child, GTK_WIDGET(label));
+   }
+    
+    void set_spinner(gboolean state)
+    {
+	if (!state){
+	    gtk_spinner_stop (pageLabelSpinner_);
+	    gtk_widget_show(GTK_WIDGET(pageLabelIconBox_));
+	    return;
+	}
+	gtk_widget_hide(GTK_WIDGET(pageLabelIconBox_));  
+	gtk_spinner_start (pageLabelSpinner_);
+    }
+    GtkBox *pageChild(void){ return pageChild_;}
+    GtkLabel *pageLabel(void){ return pageLabel_;}
+    GtkBox *pageLabelBox(void){ return pageLabelBox_;}
+    GtkSpinner *pageLabelSpinner(void){ return pageLabelSpinner_;}
+    GtkBox *pageLabelSpinnerBox(void){ return pageLabelSpinnerBox_;}
+    GtkBox *pageLabelIconBox(void){ return pageLabelIconBox_;}
+    GtkButton *pageLabelButton(void){ return pageLabelButton_;}
+private:
+    GtkBox *pageChild_;
+    GtkLabel *pageLabel_;
+    GtkBox *pageLabelBox_;
+    GtkSpinner *pageLabelSpinner_;
+    GtkBox *pageLabelSpinnerBox_;
+    GtkBox *pageLabelIconBox_;
+    GtkButton *pageLabelButton_;
+
+};
+
+
+
+
+}
+
+
+
+#endif
