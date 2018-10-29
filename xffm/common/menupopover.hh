2502e4fc89bad6e5eb5f04ed6f4de35885dacf27
edscott wilson garcia <edscott@imp.mx>
2018-10-29 11:58:07 -0600

    relocate menupopover signals

--- xffm/common/menupopover.hh
+++ xffm/common/menupopover.hh
@@ -1,16 +1,13 @@
 #ifndef MENU_POPOVER_HH
 #define MENU_POPOVER_HH
-
-#undef TRY_POPOVER
-//#define TRY_POPOVER 1
-#include "common/run.hh"
+#include "signals/menupopover.hh"
 
 namespace xf {
 template <class Type> class Notebook;
 template <class Type> class Page;
+
 template <class Type>
 class MenuPopover {
-    using run_c = Run<Type>;
 public:
     MenuPopover(void) {
 
@@ -32,44 +29,14 @@ public:
     
     GtkMenuButton *menuButton_;
     private:
-#ifdef TRY_POPOVER
-    GtkPopover *createPopover(GtkWidget *widget){
-        menuItem_t item[]={
-            {N_("Home"), (void *)home, (void *) menuButton_},
-            {N_("Open terminal"), (void *)terminal, (void *) menuButton_},
-            //{N_("Execute Shell Command"), (void *)shell, (void *) menuButton_},
-            {N_("Search"), (void *)search, (void *) menuButton_},
-            {N_("Exit"), (void *)finish, (void *) menuButton_},
-            {NULL}};
-        auto popOver = GTK_POPOVER(gtk_popover_new (widget));
-        
-        auto menuPopover = GTK_POPOVER_MENU(gtk_popover_menu_new());
-        //g_object_set(G_OBJECT(menuPopover), "name", "main", NULL);
-
-        auto p = item;
-        gint i;
-        for (i=0;p && p->label && i<1; p++,i++){
-            GtkWidget *v = gtk_menu_item_new_with_label (_(p->label));
-            gtk_container_add (GTK_CONTAINER (menuPopover), v);
-            g_signal_connect ((gpointer) v, "activate", MENUITEM_CALLBACK (p->callback), p->callbackData);
-            gtk_widget_show (v);
-        }
-
-        gtk_widget_show (GTK_WIDGET(menuPopover));
-        gtk_container_add (GTK_CONTAINER (popOver), GTK_WIDGET(menuPopover));
-        gtk_widget_show (GTK_WIDGET(popOver));
-
-        return popOver;
-    }
-#else
 
     GtkMenu *createMenu(void){
         menuItem_t item[]={
-            {N_("Home"), (void *)home, (void *) menuButton_},
-            {N_("Open terminal"), (void *)terminal, (void *) menuButton_},
-            //{N_("Execute Shell Command"), (void *)shell, (void *) menuButton_},
-            {N_("Search"), (void *)search, (void *) menuButton_},
-            {N_("Exit"), (void *)finish, (void *) menuButton_},
+            {N_("Home"), (void *)MenuPopoverSignals<Type>::home, (void *) menuButton_},
+            {N_("Open terminal"), (void *)MenuPopoverSignals<Type>::terminal, (void *) menuButton_},
+            //{N_("Execute Shell Command"), (void *)MenuPopoverSignals<Type>::shell, (void *) menuButton_},
+            {N_("Search"), (void *)MenuPopoverSignals<Type>::search, (void *) menuButton_},
+            {N_("Exit"), (void *)MenuPopoverSignals<Type>::finish, (void *) menuButton_},
             {NULL}};
         
         auto menu = GTK_MENU(gtk_menu_new());
@@ -84,81 +51,11 @@ public:
         gtk_widget_show (GTK_WIDGET(menu));
         return menu;
     }
-#endif
    /* static void
     shell(GtkMenuItem *menuItem, gpointer data)
     {
     }*/
 
-    static void
-    home(GtkMenuItem *menuItem, gpointer data)
-    {
-        auto notebook_p = (Notebook<Type> *)g_object_get_data(G_OBJECT(data), "notebook_p");
-        const gchar *path = notebook_p->workdir();
-        auto page = (Page<Type> *)notebook_p->currentPageObject();
-        page->setPageWorkdir(g_get_home_dir());
-    }
-
-    static void run(Notebook<Type> *notebook_p, const gchar *command){
-        const gchar *path = notebook_p->workdir();
-        auto page = (Page<Type> *)notebook_p->currentPageObject();
-        if (!path || !g_file_test(path, G_FILE_TEST_IS_DIR)) path = g_get_home_dir();
-	gchar *c = g_strdup(command);
-        pid_t child = run_c::thread_run(page->output(), c, FALSE);
-	page->newRunButton(c, child);
-        g_free(c);
-
-    }
-    static void
-    terminal(GtkMenuItem *menuItem, gpointer data)
-    {
-        gchar *userTerminal = NULL;
-        const gchar *terminal = getenv("TERMINAL");
-        if (terminal) {
-            userTerminal = g_strdup(terminal);
-            if (strchr(userTerminal, ' ')) *(strchr(userTerminal, ' ')) = 0;
-            gchar *g = g_find_program_in_path(userTerminal);
-            if (!g) {
-                g_free(userTerminal);
-                userTerminal = NULL;
-            } else {
-                g_free(g);
-                g_free(userTerminal);
-                userTerminal = g_strdup(terminal);
-            }
-        }
-        auto notebook_p = (Notebook<Type> *)g_object_get_data(G_OBJECT(data), "notebook_p");
-        if (userTerminal){
-            run(notebook_p, userTerminal);
-            g_free(userTerminal);
-        } else {
-            run(notebook_p, "xterm");
-        }
-    }
-
-    static void
-    search(GtkMenuItem *menuItem, gpointer data)
-    {
-        // get current directory
-        auto notebook_p = (Notebook<Type> *)g_object_get_data(G_OBJECT(data), "notebook_p");
-        const gchar *path = notebook_p->workdir();
-        if (!path || !g_file_test(path, G_FILE_TEST_IS_DIR)) path = g_get_home_dir();
-	gchar *find = g_strdup_printf("xfterm --find \"%s\"", path);
-        run(notebook_p, find);
-        g_free(find);
-    }
-
-    static void
-    finish(GtkMenuItem *menuItem, gpointer data)
-    {
-        TRACE("finish\n");
-        // The following does not retrieve the main window...
-        //GtkWidget *window = gtk_widget_get_toplevel(GTK_WIDGET(menuItem));
-        gtk_widget_hide(mainWindow);
-        while (gtk_events_pending()) gtk_main_iteration();
-        gtk_main_quit();
-        exit(0);
-    }
 
 
 };
