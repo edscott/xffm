f3c992bc5054e162a9cbe7a0bc25915232a33f95
edscott wilson garcia <edscott@imp.mx>
2018-12-12 11:34:19 -0600

    cleanup for gtk+-3.24
    No need to handle drag-motion on default iconview drop destination.
    (perhaps necessary on previous gtk versions (check this on updated gentoo distro)

--- xffm/fm/view/iconview.hh
+++ xffm/fm/view/iconview.hh
@@ -59,11 +59,11 @@ private:
 
         // "drag-motion" is not necessary with GTK_DEST_DEFAULT_MOTION
 	// while using default iconview dnd, but this is not
-	// our case.
-        g_signal_connect (G_OBJECT (baseView->destination()), 
+	// our case. But seems to make no difference qith gtk+-3.24
+        /*g_signal_connect (G_OBJECT (baseView->destination()), 
              "drag-motion", 
 	     G_CALLBACK (DragMotion),
-	     (void *)baseView);
+	     (void *)baseView);*/
         
         // source widget
         g_signal_connect (G_OBJECT (baseView->source()), 
@@ -353,7 +353,7 @@ private:
 
     } 
 
-    static gboolean
+    /*static gboolean
     DragMotion (GtkWidget * widget, 
             GdkDragContext * dc, gint drag_x, gint drag_y, 
             guint t, gpointer data) {
@@ -393,7 +393,7 @@ private:
             BaseModel<Type>::highlight(NULL, baseView);
         }
         return FALSE;
-    }
+    }*/
 
 
     static void
