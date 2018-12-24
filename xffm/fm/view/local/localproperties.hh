86fa2ebaf2a0c0f03a4b896a7842774451cf7049
Dr. Edscott Wilson Garcia <edscott.wilson.garcia@gmail.com>
2018-12-24 09:07:25 -0600

    Starting mode change widgets and methods
    Partial commit...

--- xffm/fm/view/local/localproperties.hh
+++ xffm/fm/view/local/localproperties.hh
@@ -66,6 +66,7 @@ class Properties {
 
 public:
     GtkBox *imageBox;
+
     Properties(GtkTreeModel *treeModel, GList *selectionList) {
 	
 	// copy selection list to private thread safe selection_list... 
@@ -109,6 +110,130 @@ public:
     }
     
 private:
+    /* Return a character indicating the type of file described by
+       file mode BITS:
+       'd' for directories
+       'D' for doors
+       'b' for block special files
+       'c' for character special files
+       'n' for network special files
+       'm' for multiplexor files
+       'M' for an off-line (regular) file
+       'l' for symbolic links
+       's' for sockets
+       'p' for fifos
+       'C' for contigous data files
+       '-' for regular files
+       '?' for any other file type.  */
+
+    static
+	char
+    ftypelet (mode_t bits) {
+#ifdef S_ISBLK
+	if(S_ISBLK (bits))
+	    return 'b';
+#endif
+	if(S_ISCHR (bits))
+	    return 'c';
+	if(S_ISDIR (bits))
+	    return 'd';
+	if(S_ISREG (bits))
+	    return '-';
+#ifdef S_ISFIFO
+	if(S_ISFIFO (bits))
+	    return 'p';
+#endif
+#ifdef S_ISLNK
+	if(S_ISLNK (bits))
+	    return 'l';
+#endif
+#ifdef S_ISSOCK
+	if(S_ISSOCK (bits))
+	    return 's';
+#endif
+#ifdef S_ISMPC
+	if(S_ISMPC (bits))
+	    return 'm';
+#endif
+#ifdef S_ISNWK
+	if(S_ISNWK (bits))
+	    return 'n';
+#endif
+#ifdef S_ISDOOR
+	if(S_ISDOOR (bits))
+	    return 'D';
+#endif
+#ifdef S_ISCTG
+	if(S_ISCTG (bits))
+	    return 'C';
+#endif
+
+	/* The following two tests are for Cray DMF (Data Migration
+	   Facility), which is a HSM file system.  A migrated file has a
+	   `st_dm_mode' that is different from the normal `st_mode', so any
+	   tests for migrated files should use the former.  */
+
+#ifdef S_ISOFD
+	if(S_ISOFD (bits))
+	    /* off line, with data  */
+	    return 'M';
+#endif
+#ifdef S_ISOFL
+	/* off line, with no data  */
+	if(S_ISOFL (bits))
+	    return 'M';
+#endif
+	return '?';
+    }
+
+    static gchar *
+    modeString (mode_t mode) {
+	gchar *str=(gchar *)calloc(1, 13);
+	if (!str) g_error("calloc: %s", strerror(errno));
+	str[0] = ftypelet (mode);
+	str[1] = mode & S_IRUSR ? 'r' : '-';
+	str[2] = mode & S_IWUSR ? 'w' : '-';
+	str[3] = mode & S_IXUSR ? 'x' : '-';
+	str[4] = mode & S_IRGRP ? 'r' : '-';
+	str[5] = mode & S_IWGRP ? 'w' : '-';
+	str[6] = mode & S_IXGRP ? 'x' : '-';
+	str[7] = mode & S_IROTH ? 'r' : '-';
+	str[8] = mode & S_IWOTH ? 'w' : '-';
+	str[9] = mode & S_IXOTH ? 'x' : '-';
+	if(mode & S_ISUID)
+	    str[3] = mode & S_IXUSR ? 's' : 'S';
+	if(mode & S_ISGID)
+	    str[6] = mode & S_IXGRP ? 's' : 'S';
+	if(mode & S_ISVTX)
+	    str[9] = mode & S_IXOTH ? 't' : 'T';
+	str[10] = 0;
+	return (str);
+    }
+
+    static void
+    setUpMode(GtkBox *box, entry_t *entry, mode_t newMode){
+	GList *list = gtk_container_get_children (GTK_CONTAINER(box));
+	if (!list || !list->next) return;
+	auto modeLabel = GTK_LABEL(list->data); 
+	auto modeEntry = GTK_ENTRY(list->next->data); 
+	// changed in red, unchanged in blue...
+	auto mode = entry->st.st_mode;
+	auto modeOctal = g_strdup_printf("%0o", mode);
+	auto modeText = modeString(newMode);
+//	auto modeText = modeString(entry->st.st_mode);
+	const gchar *color = (newMode == entry->st.st_mode)?"blue":"red";
+	auto modeMarkup = g_strdup_printf("<span size=\"xx-large\" color=\"%s\">%s</span>", color, modeText);
+	gtk_label_set_markup(modeLabel, modeMarkup);
+	gtk_entry_set_text(modeEntry, modeOctal+3);
+
+	g_free(modeMarkup);
+	g_free(modeOctal);
+    }
+
+    static void
+    setUpMode(GtkBox *box, entry_t *entry){
+	setUpMode(box, entry, entry->st.st_mode);
+    }
 
     static void
     setUpImage(GtkBox *box, entry_t *entry){
@@ -122,6 +247,10 @@ private:
 	}
 	gtk_container_add(GTK_CONTAINER(box), image);
 	gtk_widget_show(GTK_WIDGET(image));
+	auto modeBox = GTK_BOX(g_object_get_data(G_OBJECT(box), "modeBox"));
+	setUpMode(modeBox, entry);
+
+
     }
 
     static void
@@ -169,9 +298,37 @@ private:
 	    entry = (entry_t *)l->data;
 	    gtk_combo_box_text_append (combo, NULL, entry->basename);
 	}
-	gtk_box_pack_start(infoBox, GTK_WIDGET(combo), TRUE, FALSE, 0);
 	gtk_combo_box_set_active (GTK_COMBO_BOX(combo), 0);
 
+	auto label = GTK_LABEL(gtk_label_new(""));
+	markup = g_strdup_printf("<span size=\"xx-large\">%s</span>", 
+		_("File Mode:"));
+	gtk_label_set_markup(label, markup);
+	g_free(markup);
+	gtk_box_pack_start(infoBox, GTK_WIDGET(label), TRUE, FALSE, 0);
+
+
+	auto modeBox = GTK_BOX(gtk_box_new(GTK_ORIENTATION_VERTICAL, 1));
+	g_object_set_data(G_OBJECT(modeBox), "modeList", NULL);
+	auto modeLabel = GTK_LABEL(gtk_label_new(""));
+	auto modeEntry = GTK_ENTRY(gtk_entry_new());
+	gtk_box_pack_start(modeBox, GTK_WIDGET(modeLabel), TRUE, FALSE, 0);
+	gtk_box_pack_start(modeBox, GTK_WIDGET(modeEntry), TRUE, FALSE, 0);
+	gtk_box_pack_start(infoBox, GTK_WIDGET(modeBox), TRUE, FALSE, 0);
+	g_object_set_data(G_OBJECT(properties_p->imageBox), "modeBox", modeBox);
+	g_object_set_data(G_OBJECT(modeEntry), "modeBox", modeBox);
+	g_object_set_data(G_OBJECT(modeEntry), "modeLabel", modeLabel);
+	g_object_set_data(G_OBJECT(modeEntry), "combo", combo);
+	g_signal_connect (G_OBJECT (modeEntry), "key-release-event", G_CALLBACK (changeMode), properties_p);
+
+	label = GTK_LABEL(gtk_label_new(""));
+	markup = g_strdup_printf("<span size=\"xx-large\">%s</span>", 
+		_("Source File:"));
+	gtk_label_set_markup(label, markup);
+	g_free(markup);
+	gtk_box_pack_start(infoBox, GTK_WIDGET(label), TRUE, FALSE, 0);
+	gtk_box_pack_start(infoBox, GTK_WIDGET(combo), TRUE, FALSE, 0);
+	
 	// Add file info stuff
 	// set info stuff data according to combo box
 	// add signal handler to changed signal for combo box
@@ -200,6 +357,32 @@ private:
 	//
 	gtk_widget_show_all(GTK_WIDGET(properties_p->dialog));
 	return NULL;
+    }
+
+    static gboolean
+    changeMode (GtkEntry *modeEntry, GdkEvent  *event, void *data){
+	auto properties_p = (Properties<Type> *)data;
+	auto combo = GTK_COMBO_BOX(g_object_get_data(G_OBJECT(modeEntry),"combo"));
+	gint index = gtk_combo_box_get_active(combo);
+	GList *list = g_list_nth (properties_p->entryList,index);
+	// get entry mode
+	const gchar *text = gtk_entry_get_text(modeEntry);
+	// switch to octal
+	
+	mode_t mode;
+	sscanf(text, "%o", &mode);
+	mode &= 0777;
+
+	// update label
+	auto label = GTK_LABEL(g_object_get_data(G_OBJECT(modeEntry), "modeLabel"));
+	auto box = GTK_BOX(g_object_get_data(G_OBJECT(modeEntry), "modeBox"));
+	setUpMode(box, (entry_t *)list->data, mode);
+    
+	// compare with entry_t mode for color
+	// if equal, remove from list/hash
+	// if different replace in list/hash
+	setUpImage(properties_p->imageBox, (entry_t *)list->data);
+	return FALSE;
     }
 
     static void
