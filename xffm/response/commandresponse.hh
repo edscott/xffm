099305b8a60dc40b4e320f3baff2f4513608c068
Dr. Edscott Wilson Garcia <edscott.wilson.garcia@gmail.com>
2019-07-05 10:36:34 -0500

    move up to  52550ec377caaea2cf94a3ab4831a58b7d781819

--- xffm/response/commandresponse.hh
+++ xffm/response/commandresponse.hh
@@ -25,9 +25,9 @@ public:
 	gtk_progress_bar_set_show_text (progress, TRUE);
         gtk_progress_bar_pulse(progress);
 	gtk_widget_realize (GTK_WIDGET(dialog));
-        Response<Type>::placeDialog(dialog);
 	 
 	gtk_widget_show_all (GTK_WIDGET(dialog));
+        Response<Type>::placeDialog(dialog);
 	return dialog;
     }
     static GtkWindow *
