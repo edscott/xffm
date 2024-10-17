201d80fafe8d68f7ba5aaaf1fa2827ce93880c36
Dr. Edscott Wilson Garcia <edscott@arch0>
2024-10-17 13:20:41 -0600

    another detail on passwd dialog

--- gtk4/xffm/response/classes/passwdresponse.hh
+++ gtk4/xffm/response/classes/passwdresponse.hh
@@ -83,6 +83,7 @@ public:
       auto dialogObject = new DialogPasswd<PasswordDialog>;
       dialogObject->setParent(GTK_WINDOW(MainWidget));
       auto dialog = dialogObject->dialog();
+      MainDialog = dialog;
       auto entry = GTK_ENTRY( g_object_get_data(G_OBJECT(dialog),"entry"));
       gtk_entry_set_visibility (entry, false);
 
