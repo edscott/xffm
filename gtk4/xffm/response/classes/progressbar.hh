3a9eca4eb327fce9b77dd6ad79b6dadb2b596dd8
Dr. Edscott Wilson Garcia <edscott@kubuntu.xffm.org>
2024-12-14 18:58:58 -0600

    small warning removed

--- gtk4/xffm/response/classes/progressbar.hh
+++ gtk4/xffm/response/classes/progressbar.hh
@@ -67,7 +67,7 @@ private:
         } 
         // Complete fraction, cancel dialog and .
         if (fraction >= 1.0) {
-          auto message = g_strdup_printf(" %s (%d)\n", _("Cancelled"), _("Idle Timeout"));
+          auto message = g_strdup_printf(" %s (%s)\n", _("Cancelled"), _("Idle Timeout"));
           Print::printWarning(Child::getOutput(), message);
 
           TRACE("cancel dialog by timeout\n");
